66b5e58c0644e60662e41e7abbf0cbfcd0fd92db
Sumedh Kurhade <67695053+Diablo-web@users.noreply.github.com>
2022-11-11 13:24:30 +0000

    feat: Intital commit

---
+++ index.js
@@ -0,0 +1,99 @@
+var readlineSync = require("readline-sync");
+
+var score = 0;
+var userName = readlineSync.question("What's your name? ");
+
+console.log("Welcome " + userName + " to - DO YOU KNOW SUMEDH?");
+
+// data of high score
+var highScores = [
+   {
+    name: "Sumedh's Mom",
+    score: 9,
+  },
+  {
+    name: "Sumedh",
+    score: 10,
+  },
+  {
+    name: "Siddu",
+    score: 9,
+  }
+]
+
+// play function
+function play(question, answer) {
+  var userAnswer = readlineSync.question(question);
+
+  if (userAnswer.toUpperCase() === answer.toUpperCase()) {
+    console.log("Right! Seems like you really know Sumedh.");
+    score = score + 1;
+  } else {
+    console.log("Oops! the correct answer is " + answer);
+    console.log("And you call yourself my friend?")
+  }
+
+  console.log("Current Score: ", score);
+  console.log("-----------------")
+}
+
+// array of objects
+var questions = [
+  {
+    question: "Where do I live? ",
+    answer: "Mumbai",
+  },
+  {
+    question: "Which is my favorite anime? ",
+    answer: "One Piece",
+  },
+  {
+    question: "Who is my oldest friend? ",
+    answer: "Siddu",
+  },
+  {
+    question: "What is my favorite subject? ",
+    answer: "Maths",
+  },
+  {
+    question: "Which is my favorite cartoon character? ",
+    answer: "Doraemon",
+  },
+  {
+    question: "MCU or DC? ",
+    answer: "MCU",
+  },
+  {
+    question: "Who is my favorite MCU character? ",
+    answer: "Captain America",
+  },
+  {
+    question: "What is my favorite season? ",
+    answer: "Autumn",
+  },
+  {
+    question: "Which is my favorite drink? ",
+    answer: "Budwieser",
+  },
+  {
+    question: "Here's the last question - Who is my favorite cricket player? ",
+    answer: "Rohit Sharma",
+  }
+]
+
+// loop
+for (var i = 0; i < questions.length; i++) {
+  play(questions[i].question, questions[i].answer);
+}
+
+console.log("YAY! You SCORED: ", score);
+console.log("-------------------")
+
+console.log("Check out the high scorers - ");
+for (var i = 0; i < highScores.length; i++) {
+  console.log(highScores[i].name + ": " + highScores[i].score);
+}
+console.log("-------------------")
+
+console.log("If you have scored better than the current high scorers then send me a screenshot of your score, so that I can add you to the high scorers list.")
+console.log("Thank you for playing!")
